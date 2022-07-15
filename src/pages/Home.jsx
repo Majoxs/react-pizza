f9d2e9d119d24e0b2263028f15f391423f7e8c3d
Majoxs <majoxs@gmail.com>
2022-07-15 17:50:17 +0500

    Implementations sorting and filtering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,23 +8,35 @@ import Skeleton from '../components/PizzaBlock/Skeleton';
 const Home = () => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortType, setSortType] = React.useState({
+    name: 'популярности',
+    sortProperty: 'rating',
+  });
 
   React.useEffect(() => {
-    fetch('https://62cec110826a88972d025b9d.mockapi.io/items')
+    setIsLoading(true);
+
+    const sortBy = sortType.sortProperty.replace('-', '');
+    const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+    const category = categoryId > 0 ? `category=${categoryId}` : '';
+
+    fetch(
+      `https://62cec110826a88972d025b9d.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+    )
       .then((response) => response.json())
       .then((data) => {
         setItems(data);
         setIsLoading(false);
       });
-
     window.scrollTo(0, 0);
-  }, []);
+  }, [categoryId, sortType]);
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories value={categoryId} onChangeCategory={(i) => setCategoryId(i)} />
+        <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
